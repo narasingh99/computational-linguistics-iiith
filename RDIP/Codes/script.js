16705b7782739b784dd22ebdca02991c08f74887
narasingh99 <narasingh1999@gmail.com>
2020-07-19 20:18:55 +0530

    check correctness

--- RDIP/Codes/script.js
+++ RDIP/Codes/script.js
@@ -16,6 +16,7 @@ var hin = [["राम और श्याम बाजार गयें","र
 ["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
 ["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
 ["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]]; 
+var combinedarray = ["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम","राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी","मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम","राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम","बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को","एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब","एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब","John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple","some students like to study in the night","at night some students like to study","John and Mary went to church","Mary and John went to church","John went to church after eating","after eating John went to church","John after eating went to church","did he go to market","he did go to market","the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics", "John goes to the library and studies","John studies and goes to the library", "John ate an apple so did she","she ate an apple so did John", "the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book", "I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her" ];
 var buttoncount=0;
 var wordcount=0;
 function sentencefunc()
@@ -63,6 +64,7 @@ alert("select language");
 return false;
 }
 }
+var originalsentence = "";
 var initialvalues ="";
 function englishsentence()
 {
@@ -126,14 +128,14 @@ var finishedsentence = "";
 function display(id,value)
 {
 document.getElementById("demo3").innerHTML = "Formed Sentence (after selecting words):";
-finishedsentence += value + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+finishedsentence += value + " ";
 document.getElementById("demo4").innerHTML = finishedsentence;
 document.getElementById(id).style.display = "none";
 document.getElementById("demo6").innerHTML = "<center><button id='reform' onclick='resets(initialvalues)'>Re-form the sentence</button></center>"
 wordcount++;
 if(wordcount == buttoncount)
 {
-document.getElementById("demo7").innerHTML = "<center><button id='check' onclick='checkfunc(finishedsentence)'>Check the correctness of sentence</button></center>"
+document.getElementById("demo7").innerHTML = "<center><button id='check' onclick='checkfunc()'>Check the correctness of sentence</button></center>"
 }
 return true;
 }
@@ -144,12 +146,31 @@ function resets(initialvalues)
 document.getElementById("demo3").innerHTML="";
 document.getElementById("demo4").innerHTML="";
 document.getElementById("demo6").innerHTML ="";  
+document.getElementById("demo7").innerHTML = "";
+document.getElementById("demo8").innerHTML = "";
+document.getElementById("demo9").innerHTML = "";
 wordcount=0;
 document.getElementById("ans").innerHTML = initialvalues;
 finishedsentence = "";
 return true;
 }
 function checkfunc()
+{
+var str = finishedsentence.trim();
+for(var j=0;j<combinedarray.length;j++){
+        var str1 = combinedarray[j];
+        var n = str1.localeCompare(str);
+console.log(j, str1.localeCompare(str), str, str.length, str1, str1.length)
+        if (n == 0) {
+            document.getElementById("demo8").innerHTML = "RIGHT ANSWER";
+
+            return;
+        }
+     }
+    document.getElementById('demo9').innerHTML = "WRONG ANSWER";
+    document.getElementById("demo10").innerHTML = "<center><button id='showansbtn' onclick='answers()'>Get Correct Sentence</button></center>"
+}
+function answers()
 {
 
 }
